07a8e16eb02fec8593581b29925dc2ad8550bdc9
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-10-16 10:37:23 +0900

    feat: add simple counter class

---
+++ src/commonMain/kotlin/Counter.kt
@@ -0,0 +1,25 @@
+
+class Counter(initValue: Int, min: Int, max: Int) {
+    var value : Int = initValue
+        private set
+
+    var max : Int = max
+        private set
+
+    var min : Int = min
+        private set
+
+    init {
+        check(min <= max) { "invalid parameter.(min or max)" }
+    }
+
+    fun increment() {
+        if (value == max) return
+        value++
+    }
+
+    fun decrement() {
+        if (value == min) return
+        value--
+    }
+}
